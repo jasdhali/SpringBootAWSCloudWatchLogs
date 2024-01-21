7b67e5e95a9263018da0d2d2c3d01d275c92d8df
Jaspal <jasdhali@gmail.com>
2024-01-21 12:39:35 -0600

    Sample boot to write cloudwatch logs from EC2 instance

---
+++ src/main/java/com/aws/clouwatcghlogs/SpringBootAwsCloudWatchLogsApplication.java
@@ -0,0 +1,13 @@
+package com.aws.clouwatcghlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsCloudWatchLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsCloudWatchLogsApplication.class, args);
+	}
+
+}
